3c4435d8e46a11396eb96ecd2f080ec9487d08eb
bryanbalsley <webbarbarian@hotmail.com>
2022-03-01 13:08:13 -0800

    test

---
+++ app/src/components/profile/Profile.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from './React';
+
+
+const Profile = () => {
+
+    const [profileData, setProfileData] = useState();
+
+    
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Profile;
